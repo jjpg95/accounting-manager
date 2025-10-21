c83df1b680268647dac59a3b028ba4a1cb2e8a58
Jose Juan Perez Gonzalez <pepeju95@gmail.com>
2025-10-21 19:22:15 +0200

    Add modal fom to add new register

--- src/app/Dashboard/Dashboard.tsx
+++ src/app/Dashboard/Dashboard.tsx
@@ -1,44 +1,62 @@
-'use client'
+"use client";
 
-import { BarChart } from "@mui/x-charts"
+import { useRef, useState } from "react";
+import { Modal } from "../components/modal";
+
+const INPUT_CLASSES = "border rounded-md p-2 max-w-40";
 
 export default function Dashboard() {
-	const data = [
-		{
-			income: 1000,
-			outcome: 300,
-			balance: 700,
-			date: '18/10/2024'
-		},
-		{
-			income: 1000,
-			outcome: 1500,
-			balance: -500,
-			date: '19/10/2024'
-		},
-		{
-			income: 2500,
-			outcome: 800,
-			balance: 1700,
-			date: '20/10/2024'
-		}
-	]
-	return <main className="flex flex-col sm:flex-row flex-wrap pt-10">
-		<section className="w-full sm:w-1/2 pb-10">
-			<BarChart
-				dataset={data}
-				xAxis={[{ scaleType: 'band', dataKey: 'date' }]}
-				series={[
-					{ dataKey: 'income', label: 'Income' },
-					{ dataKey: 'outcome', label: 'Outcome' },
-					{ dataKey: 'balance', label: 'Balance' }
-				]}
-				width={500}
-				height={500}
-			/>
-		</section>
-		<section className="w-full sm:w-1/2 pb-10">
-			Daily balance form
-		</section>
-	</main>
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [totalNet, setTotalNet] = useState(0);
+  const liquidReference = useRef<HTMLInputElement>(null);
+  const targetReference = useRef<HTMLInputElement>(null);
+  const expensesReference = useRef<HTMLInputElement>(null);
+
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  const handleCloseModal = () => {
+    setTotalNet(0);
+    setIsModalOpen(false);
+  };
+
+  const handleNetChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const liquidValue = parseFloat(liquidReference.current?.value || "0");
+    const targetValue = parseFloat(targetReference.current?.value || "0");
+    const expensesValue = parseFloat(expensesReference.current?.value || "0");
+    const netTotal = liquidValue + targetValue - expensesValue;
+    setTotalNet(netTotal);
+  };
+
+  return (
+    <main className="flex flex-col sm:flex-row flex-wrap pt-10 justify-center">
+      <section className="flex flex-col w-full sm:w-1/2 pb-10 items-center gap-4">
+        <button onClick={handleOpenModal}>Añadir registro</button>
+        <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+          <form className="grid grid-cols-2 sm:grid-cols-1 gap-4">
+            <input
+              className={INPUT_CLASSES}
+              placeholder="Efectivo"
+              onChange={handleNetChange}
+              ref={liquidReference}
+            />
+            <input
+              className={INPUT_CLASSES}
+              placeholder="Tarjeta"
+              onChange={handleNetChange}
+              ref={targetReference}
+            />
+            <input
+              className={INPUT_CLASSES}
+              placeholder="Gastos"
+              onChange={handleNetChange}
+              ref={expensesReference}
+            />
+            <input className={INPUT_CLASSES} value={totalNet} disabled={true} />
+          </form>
+        </Modal>
+      </section>
+    </main>
+  );
 }
